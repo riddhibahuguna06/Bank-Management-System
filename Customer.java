b35f0f9348f7ea25fc60f4f990a1e9d5b50de0c4
riddhibahuguna06 <riddhibahuguna@gmail.com>
2026-03-14 21:19:08 +0530

    Added Customer , Bank , Account , Main , Transaction class and constructor in Customer class

---
+++ Customer.java
@@ -0,0 +1,13 @@
+public class Customer{
+    private int accountNumber;
+    private String name;
+    private String mobileNumber;
+    
+    public Customer(int accountNumber , String name , String mobileNumber){
+     this.accountNumber = accountNumber ;
+     this.mobileNumber = mobileNumber;
+     this.name = name;
+    }
+
+    
+}
