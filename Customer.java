695cd4d8eef7b82c55ba76566e72d7b293b89d0c
riddhibahuguna06 <riddhibahuguna@gmail.com>
2026-03-17 16:39:26 +0530

    Added customerid method

--- Customer.java
+++ Customer.java
@@ -3,12 +3,18 @@ public class Customer{
     private String name;
     private String mobileNumber;
     private Account account;
+    private String customerId ;
     
-    public Customer( String name , String mobileNumber , Account account){
+    public Customer( String name , String mobileNumber , Account account , String customerId){
     
      this.mobileNumber = mobileNumber;
      this.name = name;
      this.account = account ;
+     this.customerId = customerId ;
+    }
+
+    public String getCustomerId(){
+        return customerId ;
     }
 
     public Account getAccount(){
@@ -26,6 +32,7 @@ public class Customer{
     public void getCustomerDetails(){
     System.out.println("Name of the customer: " + getName());
     System.out.println("Mobile number of the customer: " + getMobileNum());
+    System.out.println("Your customer ID: " + getCustomerId());
     System.out.println("Your Account details: ");
      account.getAccountDetails();
     }
