84323366d2af2c71d16b8d98b0a3121f571cfa4a
riddhibahuguna06 <riddhibahuguna@gmail.com>
2026-03-18 22:03:32 +0530

    Complted my switch

--- Main.java
+++ Main.java
@@ -7,6 +7,11 @@ Bank bank = new Bank() ;
 
 while(true){
 
+     String accountNumber = "";
+     Customer c = null ;
+     double amount = 0 ;
+
+
     System.out.println("-------------Menu------------");
     System.out.println("1. Deposit\n" +
                       "2. Withdraw\n" +
@@ -19,11 +24,9 @@ while(true){
     int choice = sc.nextInt() ;
     sc.nextLine();
 
-    String accountNumber = "";
-    Customer c = null ;
-    double amount = 0 ;
 
     switch (choice) {
+
         case 1: // Deposit
         System.out.println("Your Account number: ");
         accountNumber = sc.next();
@@ -49,15 +52,44 @@ while(true){
             break;
 
         case 3: // transaction history
+        System.out.println("Your Account Number: ");
+        accountNumber = sc.next();
+        sc.nextLine();
+
+        bank.viewTransactionHistory(accountNumber) ;
+            break;
+
+        case 4 : // Add new account
+
+        System.out.println("Your name : ");
+        String name = sc.next();
+        sc.nextLine();
+
+        System.out.println("Your mobile number: ");
+        String mobileNumber = sc.next();
+        sc.nextLine();
+
+        System.out.println("Your Account type: ");
+        String accountType = sc.next();
+        sc.nextLine() ;
+
+        System.out.println("Balance you want add to your account: ");
+        double balance = sc.nextDouble();
+       
+        bank.addCustomer(name, mobileNumber, balance , accountType);
             
             break;
 
-        case 4:
-            
+         case 5: // viewAccounts
+
+            bank.viewAccounts();
             break;
 
-        case 5 :
-            
+
+        case 6 :
+         System.out.println("Goodbye!");   //exit 
+                sc.close();   
+
             break;
         default:
             break;
